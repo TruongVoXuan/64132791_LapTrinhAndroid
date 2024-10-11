d599a3ad0c370a5d4b09e6d651ee641e742181c2
TruongVoXuan <162585350+TruongVoXuan@users.noreply.github.com>
2024-10-11 12:20:46 +0700

    code BMI

---
+++ app/src/main/java/com/example/appbmi/AppBMI.java
@@ -0,0 +1,29 @@
+package com.example.appbmi;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.helloworld.R;
+
+public class AppBMI extends AppCompatActivity {
+    Button btnCalculate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_app_bmi);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            btnCalculate = findViewById(R.id.btnCalculate);
+            return insets;
+        });
+    }
+}
