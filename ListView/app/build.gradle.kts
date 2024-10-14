097d11b3f7eab48e1dce2b0f0d051e546b72fdbe
TruongVoXuan <truong.75.cvan@gmail.com>
2024-10-14 22:00:53 +0700

    List1

--- ListView/app/build.gradle.kts
+++ ListView/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.helloworld"
+    namespace = "com.example.listview"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.helloworld"
+        applicationId = "com.example.listview"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
--- ListView/app/src/test/java/com/example/listview/ExampleUnitTest.java
+++ ListView/app/src/test/java/com/example/listview/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.listview1;
+package com.example.listview;
 
 import org.junit.Test;
 
