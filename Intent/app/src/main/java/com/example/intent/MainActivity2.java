2ee3ce6c5b61a3ecd1f7cfc2051e601149c6cec0
TruongVoXuan <truong.75.cvan@gmail.com>
2024-10-25 14:17:23 +0700

    ChangeScreen

--- Intent/app/src/main/java/com/example/intent/MainActivity2.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity2.java
@@ -3,42 +3,43 @@ package com.example.intent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-/*CÁC BƯỚC:
-            BÊN activity1
-        1.Tạo đối tượng Intent
-        2.Gói dữ liệu vào
-        3. +startActivity
-        or startActivity forResult
-        4. onActivityResult
-            BÊN activity2
-        1. getIntent()
-        2.Bóc lấy data
-        3.setResult()*/
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public  void ChuyenManHinh(View view){
-            //1.Tạo intent
-        Intent iMH = new Intent(MainActivity.this, MainActivity2.class);
-            //2. Gói dữ liệu
-        iMH.putExtra("ten","Vo Xuan Truong");  //Láy dữ liệu màn đầu hiện ra màn 2
-            //3. Swaps màn hình
-        startActivity(iMH);
+
+    public  void ChuyenLaiMan(View view){
+        //1. Lấy intent về
+
+        Intent iNhanDuoc = getIntent();
+
+        //2.Bốc dữ liệu
+
+        String data = iNhanDuoc.getStringExtra("ten");
+
+        //3.Hiển lên màn
+
+        Toast.makeText(getBaseContext(), data, Toast.LENGTH_SHORT).show();
+
+        //4.Chuyển về màn hình chính
+        Intent iManHinhChinh = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(iManHinhChinh);
     }
 }
