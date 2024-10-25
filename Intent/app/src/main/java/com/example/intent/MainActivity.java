362ff0916bc2f87cec4a66496088a356bc818b2f
TruongVoXuan <truong.75.cvan@gmail.com>
2024-10-25 13:52:05 +0700

    up

--- Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.intent;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,5 +31,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public  void ChuyenManHinh(View view){
+
     }
 }
