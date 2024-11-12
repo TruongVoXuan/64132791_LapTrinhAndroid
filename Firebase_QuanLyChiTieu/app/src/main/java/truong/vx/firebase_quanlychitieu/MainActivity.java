84550a3521a9975dda051db4d13692f8650ddf79
TruongVoXuan <truong.75.cvan@gmail.com>
2024-11-12 15:18:14 +0700

    Update MainActivity.java

--- Firebase_QuanLyChiTieu/app/src/main/java/truong/vx/firebase_quanlychitieu/MainActivity.java
+++ Firebase_QuanLyChiTieu/app/src/main/java/truong/vx/firebase_quanlychitieu/MainActivity.java
@@ -1,12 +1,19 @@
 package truong.vx.firebase_quanlychitieu;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");  //key
+
+        myRef.setValue("Hello, World!");  // value
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                //Xu ly du lieu
+                Toast.makeText(getBaseContext(), "Value is: " +
+                        value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(getBaseContext(), "Failed to read value " 
+                       , Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
