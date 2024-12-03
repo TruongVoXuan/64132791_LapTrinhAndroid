e509acf4738aa0bd4984028f421fda18c0e3a2f1
TruongVoXuan <truong.75.cvan@gmail.com>
2024-12-03 15:28:52 +0700

    BaiTH2

--- TH_Bai2_BasicGUI_BMIAssignment/app/src/main/java/truong/vx/th_bai2_basicgui_bmiassignment/MainActivity.java
+++ TH_Bai2_BasicGUI_BMIAssignment/app/src/main/java/truong/vx/th_bai2_basicgui_bmiassignment/MainActivity.java
@@ -1,24 +1,102 @@
 package truong.vx.th_bai2_basicgui_bmiassignment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText heightEditText, weightEditText;
+    TextView resultTextView;
+    Button calculateButton;
+    RadioGroup radioGroupStandard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        heightEditText = findViewById(R.id.editTextHeight);
+        weightEditText = findViewById(R.id.editTextWeight);
+        resultTextView = findViewById(R.id.textViewResult);
+        calculateButton = findViewById(R.id.buttonCalculate);
+        radioGroupStandard = findViewById(R.id.radioGroupStandard);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateBMI();
+            }
         });
+    }
+
+    private void calculateBMI() {
+        double height, weight, bmi;
+
+        try {
+            height = Double.parseDouble(heightEditText.getText().toString()) / 100;
+            weight = Double.parseDouble(weightEditText.getText().toString());
+            bmi = weight / (height * height);
+
+
+            String bmiCategory;
+
+
+
+            int selectedID = radioGroupStandard.getCheckedRadioButtonId();
+
+            if(selectedID == R.id.radioWHO)
+            {
+                if (bmi < 18.5) {
+                    bmiCategory = "Gầy";
+
+                } else if (bmi >= 18.5 && bmi < 24.9) {
+                    bmiCategory = "Bình thường";
+
+                } else if (bmi >= 25 && bmi < 29.9) {
+                    bmiCategory = "Thừa cân";
+
+                } else {
+                    bmiCategory = "Béo phì";
+
+                }
+            }
+            else if (selectedID == R.id.radioASIA)
+            {
+                if (bmi < 17.5) {
+                    bmiCategory = "Gầy";
+
+                } else if (bmi >= 17.5 && bmi < 22.9) {
+                    bmiCategory = "Bình thường";
+
+                } else if (bmi >= 23 && bmi < 27.9) {
+                    bmiCategory = "Thừa cân";
+
+                } else {
+                    bmiCategory = "Béo phì";
+
+                }
+            }
+            else {
+                bmiCategory = "hãy chọn tiêu chuẩn";
+            }
+
+
+
+
+            resultTextView.setText(String.format("BMI của bạn: %.2f - %s", bmi, bmiCategory));
+
+
+        } catch (NumberFormatException e) {
+            resultTextView.setText("Nhập số hợp lệ cho chiều cao và cân nặng.");
+            resultTextView.setTextColor(Color.GRAY); // Màu xám cho lỗi nhập liệu
+        }
     }
 }
